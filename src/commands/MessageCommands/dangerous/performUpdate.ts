bf95f78094247058996da248e00bf4e74a8f874b
Ascynx <miaoum2128@gmail.com>
2023-12-04 15:14:54 +0100

    package fix

---
+++ src/commands/MessageCommands/dangerous/performUpdate.ts
@@ -0,0 +1,57 @@
+import { Command } from "../../../structures/Commands";
+import { exec, execSync } from "child_process";
+import { escape } from "querystring";
+
+export default new Command({
+    name: "update",
+    aliases: ["pu", "u"],
+    devonly: true,
+    category: "other",
+    usage: `${process.env.botPrefix} update <branch?>`,
+
+    run: async ({ client, message, args, request }) => {
+        let branch = "main";
+        if (args.length == 1) {
+            branch = args[0];
+        }
+
+        exec("git branch", (execErr, stdout, stderr) => {
+            if (execErr) {
+                request.send({content: `Failed to update: ` + execErr});
+                return;
+            }
+
+            let splits = stdout.split("\n");
+
+            let currBranch = "main";
+            for (let split of splits) {
+                //transformations
+                split.toLowerCase();
+                split.trimEnd();
+
+                if (!split.startsWith("*")) {
+                    continue;
+                }
+
+                currBranch = split;
+            }
+
+            if (currBranch != branch) {
+                branch.replaceAll(/[^a-zA-Z0-9_\-\./]/gmi, "");
+                let checkoutStdOut = execSync("git checkout " + escape(branch));
+                if (checkoutStdOut.toString('utf-8', 0, 5).toLowerCase() == "error") {
+                    request.send("Failed to update, could not checkout to branch " + branch);
+                    return;
+                }
+            }
+
+            let pullStdOut = execSync("git pull");
+            if (pullStdOut.toString('utf-8').toLowerCase() == "already up to date.") {
+                request.send("Local instance is currently up to date");
+            }
+
+            let buildStdOut = execSync("npm run build");
+            execSync("pm2 restart ")
+        })
+    }
+});
